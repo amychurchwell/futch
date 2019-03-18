e315c8fb16a1a701fc8c3a0e9bc5648cf5ef943f
juniper wilde <juniper.r.wilde@gmail.com>
2019-03-17 22:44:52 -0700

    clean up jsx warnings

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,7 @@ export default () => (
   <div>
     <p>this was a webpage</p>
     <iframe
+      title='abelian'
       width='100%'
       id='abelian-webring-nav'
       src='https://abelian.now.sh/embed/'
