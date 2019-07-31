fe0bdd04a513d8a51b8e23291ce3c5046877476e
juniper wilde <juniper.r.wilde@gmail.com>
2019-07-31 16:25:31 -0700

    add aria-labels to emoji

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -12,22 +12,45 @@ export default () => (
       </Link>
     </div>
     <div className={sidebarStyles.div_two}>
-      <Link to="/">⚜️ home</Link>
+      <Link to="/">
+        <span role="img" aria-label="fleur de lis">
+          ⚜️
+        </span>
+        home
+      </Link>
     </div>
     <div className={sidebarStyles.div_three}>
-      <Link to="/love">💛 love</Link>
+      <Link to="/love">
+        <span role="img" aria-label="yellow heart">
+          💛
+        </span>
+        love
+      </Link>
     </div>
     <div className={sidebarStyles.div_four}>
-      <Link to="/blog">🗒️ blog</Link>
+      <Link to="/blog">
+        <span role="img" aria-label="spiral notepad">
+          🗒️
+        </span>
+        blog
+      </Link>
     </div>
     <div className={sidebarStyles.div_five}>
-      <Link to="/work">⚒️ work</Link>
+      <Link to="/work">
+        <span role="img" aria-label="hammer and pick">
+          ⚒️
+        </span>
+        work
+      </Link>
     </div>
     <div className={sidebarStyles.div_six}>
-      <Link to="/about">👩‍💻 about</Link>
+      <Link to="/about">
+        <span role="img" aria-label="woman technologist: light skin tone">
+          👩🏻‍💻
+        </span>
+        about
+      </Link>
     </div>
-    <div className={sidebarStyles.div_seven}>
-      copyright juniper wilde 2019
-    </div>
+    <div className={sidebarStyles.div_seven}>copyright juniper wilde 2019</div>
   </div>
 );
