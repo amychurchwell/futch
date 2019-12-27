772bcaa794165552b1ebdea9cac30e74c785836b
juniper wilde <juniper.r.wilde@gmail.com>
2019-12-27 13:43:14 -0800

    update blog listing styles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -14,12 +14,13 @@ export default (props) => {
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug;
           return (
-            <div key={node.fields.slug}>
-              <h3>
+            <div className="post" key={node.fields.slug}>
+              <div className="postTitle">
                 <Link to={node.fields.slug}>{title}</Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p
+                <span> - </span>
+                <span className="postDate">{node.frontmatter.date}</span>
+              </div>
+              <div className="postExcerpt"
                 dangerouslySetInnerHTML={{
                   __html: node.frontmatter.description || node.excerpt,
                 }}
